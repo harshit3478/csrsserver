3b4efa0a81c21756209d862038b74c8c92a0361e
Harry <harsagra3478@gmail.com>
2024-01-12 12:54:21 +0530

    user ID is not required anymore

--- mongoose/User.js
+++ mongoose/User.js
@@ -5,7 +5,7 @@ const UserDetails = new mongoose.Schema(
 	username : {type : String , required : true , minlength:4}, 
 	password : {type: String , required : true , minlength:6 },
 	email : {type:String , required:true , unique:true 	},
-	userId : {type:String , required:true , unique:true },
+	userId : {type:String , unique:true },
 	phone : {type:String ,  unique:true 	},
 	contacts: [
 		{
