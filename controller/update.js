12bb18b4dd770aceff0723238edcea3639dc908a
Harry <harsagra3478@gmail.com>
2024-03-01 20:26:21 +0530

    update api created for token refresh

--- controller/update.js
+++ controller/update.js
@@ -1,19 +1,17 @@
 const { uploadToCloudinary, removeFromCloudinary } = require('./cloudinary.js');
 const { User } = require('../mongoose/User.js');
-exports.update = async(req, res) => {
-    const {email , name  } = req.body;
+exports.updateToken = async(req, res) => {
+    const {email , token } = req.body;
     console.log(" email is ", email)
     try{
       const user = await User.findOne({email : email});
       if(!user) return res.status(411).send({status : 'error' , message : 'user not found',  code : -1});
-        const data = await uploadToCloudinary(req.files.image , "user-images");
+       
       const updatedUser = await User.updateOne({
         email : email ,
       },{
         $set : {
-          imageUrl : data.url,
-          publicId : data.public_id,
-          username : name
+         userId : token
         }
       });
       res.status(200).send({status : 'success' , message : 'user updated',  code : 1});
