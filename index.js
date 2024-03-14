6a32d286acd01d2abf3244989cd9f7ad6fe34490
Harry <harsagra3478@gmail.com>
2024-03-14 22:24:25 +0530

    silly silly

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const io = new Server(server ,
   {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://csrsweb.vercel.app",
       methods: ["GET", "POST"]
     }
   });
